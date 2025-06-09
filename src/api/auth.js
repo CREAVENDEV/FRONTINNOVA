ebe96b736193d48cd6d7567f2d8a139c13d66ed7
Omar Crespo <omar.crespo@seconad.com>
2025-06-09 17:18:23 -0600

    Deploy 200 Front

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const API = "http://127.0.0.1:8000/myapp/register/";
+const API = "https://backinnova.onrender.com/myapp/register/";
 export const registerrequest = user => axios.post(`${API}`, user)
 
-const API2 = "http://127.0.0.1:8000/myapp/login/";
+const API2 = "https://backinnova.onrender.com/myapp/login/";
 export const loginRequest = user => axios.post(`${API2}`, user);
 
 
